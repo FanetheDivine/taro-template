0d80a3d09fd04f64c2cb4f840472c71f75dddb18
Yuanzhuoxin <1974269607@qq.com>
2024-08-11 00:29:54 +0800

    chore: 样式和文档

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ taro+tailwindcss项目模板.引入taro-hook v2.
 * 关闭微信开发者工具热重载
 * 推荐使用pnpm
 * 路由使用[taro-app-router](https://github.com/FanetheDivine/taro-app-router/blob/main/README.md)
+* 图片,gif等存在类型问题 这是来自Taro的bug
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
-import React, { useCallback, useState } from "react";
-import { View, Text, Button, Image } from "@tarojs/components";
+import { FC, useCallback, useState } from "react";
+import { View, Text, Button } from "@tarojs/components";
 import { useEnv, useNavigationBar, useModal, useToast } from "taro-hooks";
-import logo from "./hook.png";
 
-import "./index.scss";
-
-const Index = () => {
+const Index: FC = () => {
   const env = useEnv();
   const [num, setNum] = useState(1);
-  const { setTitle } = useNavigationBar({ title: "Taro Hooks" });
+  const { setTitle } = useNavigationBar();
   const showModal = useModal({
     title: "Taro Hooks Canary!",
     showCancel: false,
@@ -24,15 +21,9 @@ const Index = () => {
   }, [show, showModal]);
 
   return (
-    <View className="wrapper">
+    <View className="w-full h-full flex flex-col items-start">
       <Button onClick={() => setNum(num + 1)}>{num}</Button>
-      <Image className="logo" src={logo} />
       <Text className="text-red-300">为Taro而设计的Hooks Library</Text>
-      <Text className="desc">
-        目前覆盖70%官方API. 抹平部分API在H5端短板. 提供近40+Hooks!
-        并结合ahook适配Taro! 更多信息可以查看新版文档:
-        https://next-version-taro-hooks.vercel.app/
-      </Text>
       <View className="list">
         <Text className="label">运行环境</Text>
         <Text className="note">{env}</Text>
